ffa6d73bd51a628ad00326a282801579040d8153
Raúl Vidal Muiños <raul.vidal@devinet.es>
2025-03-31 09:40:25 +0200

    Changes

--- DVNTUIButtonDesignableExtension.podspec
+++ DVNTUIButtonDesignableExtension.podspec
@@ -1,17 +1,17 @@
 Pod::Spec.new do |s|
   
   s.name             = 'DVNTUIButtonDesignableExtension'
-  s.version          = '1.0.4'
+  s.version          = '1.1.0'
   s.summary          = 'An amazing UIButton estension.'
   s.description      = 'A extension that let\'s you set the corner radius, the border with and the border color of a button trought Interface Builder.'
   s.homepage         = 'https://www.devinet.es'
-  s.license          = { :type => 'Copyright (c) 2019 Devinet 2013, S.L.U.', :file => 'LICENSE' }
+  s.license          = { :type => 'Copyright (c) 2025 Devinet 2013, S.L.U.', :file => 'LICENSE' }
   s.author           = { 'Raúl Vidal Muiños' => 'contacto@devinet.es' }
   s.social_media_url = 'https://twitter.com/devinet_es'
   s.platform         = :ios, '9.3'
   s.ios.deployment_target = '9.3'
-  s.swift_versions   = ['3.0', '4.0', '4.1', '4.2', '5.0']
-  s.source           = { :git => 'https://bitbucket.org/Devinet_Team/ios-library-dvntuibuttondesignableextension.git', :tag => s.version.to_s }
+  s.swift_versions   = ['3.0', '4.0', '4.1', '4.2', '5.0',  '5.1',  '5.2',  '5.3',  '5.4',  '5.5',  '5.6',  '5.7',  '5.8',  '5.9',  '5.10']
+  s.source           = { :git => 'https://github.com/Devinet-2013-S-L-U/ios-library-dvntuibuttondesignableextension.git', :tag => s.version.to_s }
   s.frameworks       = 'UIKit'
   s.source_files     = 'Sources/DVNTUIButtonDesignableExtension/Classes/**/*'
   s.exclude_files    = 'Sources/DVNTUIButtonDesignableExtension/**/*.plist'
--- Sources/DVNTUIButtonDesignableExtension/Classes/DVNTUIButtonDesignableExtension.swift
+++ Sources/DVNTUIButtonDesignableExtension/Classes/DVNTUIButtonDesignableExtension.swift
@@ -5,35 +5,149 @@
 //
 
 import UIKit
+import ObjectiveC
 
-@IBDesignable extension UIButton
-{
+private var gradientLayerKey: UInt8 = 0
+private var gradientStartColorKey: UInt8 = 0
+private var gradientEndColorKey: UInt8 = 0
+
+@IBDesignable extension UIButton {
+        
     @IBInspectable var borderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
-        get {
-            return layer.borderWidth
-        }
+        set { layer.borderWidth = newValue }
+        get { layer.borderWidth }
     }
     
     @IBInspectable var cornerRadius: CGFloat {
         set {
             layer.cornerRadius = newValue
+            // Actualiza el gradiente si existe
+            gradientLayer?.cornerRadius = newValue
         }
-        get {
-            return layer.cornerRadius
-        }
+        get { layer.cornerRadius }
     }
     
     @IBInspectable var borderColor: UIColor? {
         set {
-            guard let uiColor = newValue else { return }
-            layer.borderColor = uiColor.cgColor
+            guard let color = newValue else { return }
+            layer.borderColor = color.cgColor
         }
         get {
-            guard let color = layer.borderColor else { return nil }
-            return UIColor(cgColor: color)
+            guard let cgColor = layer.borderColor else { return nil }
+            return UIColor(cgColor: cgColor)
         }
     }
+    
+    // MARK: - Sombras
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            if let cgColor = layer.shadowColor {
+                return UIColor(cgColor: cgColor)
+            }
+            return nil
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get { layer.shadowOpacity }
+        set { layer.shadowOpacity = newValue }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get { layer.shadowOffset }
+        set { layer.shadowOffset = newValue }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get { layer.shadowRadius }
+        set { layer.shadowRadius = newValue }
+    }
+    
+    // MARK: - Content Insets
+    @IBInspectable var contentInsetTop: CGFloat {
+        get { contentEdgeInsets.top }
+        set {
+            var insets = contentEdgeInsets
+            insets.top = newValue
+            contentEdgeInsets = insets
+        }
+    }
+    
+    @IBInspectable var contentInsetLeft: CGFloat {
+        get { contentEdgeInsets.left }
+        set {
+            var insets = contentEdgeInsets
+            insets.left = newValue
+            contentEdgeInsets = insets
+        }
+    }
+    
+    @IBInspectable var contentInsetBottom: CGFloat {
+        get { contentEdgeInsets.bottom }
+        set {
+            var insets = contentEdgeInsets
+            insets.bottom = newValue
+            contentEdgeInsets = insets
+        }
+    }
+    
+    @IBInspectable var contentInsetRight: CGFloat {
+        get { contentEdgeInsets.right }
+        set {
+            var insets = contentEdgeInsets
+            insets.right = newValue
+            contentEdgeInsets = insets
+        }
+    }
+    
+    // MARK: - Gradiente de fondo
+    
+    private var gradientLayer: CAGradientLayer? {
+        get { objc_getAssociatedObject(self, &gradientLayerKey) as? CAGradientLayer }
+        set { objc_setAssociatedObject(self, &gradientLayerKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    @IBInspectable var gradientStartColor: UIColor? {
+        get { objc_getAssociatedObject(self, &gradientStartColorKey) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &gradientStartColorKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            updateGradientLayer()
+        }
+    }
+    
+    @IBInspectable var gradientEndColor: UIColor? {
+        get { objc_getAssociatedObject(self, &gradientEndColorKey) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &gradientEndColorKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            updateGradientLayer()
+        }
+    }
+    
+    private func updateGradientLayer() {
+        guard let startColor = gradientStartColor, let endColor = gradientEndColor else {
+            // Si no se han establecido ambos colores, se elimina el gradiente.
+            gradientLayer?.removeFromSuperlayer()
+            gradientLayer = nil
+            return
+        }
+        
+        if let gradLayer = gradientLayer {
+            gradLayer.frame = bounds
+            gradLayer.colors = [startColor.cgColor, endColor.cgColor]
+            gradLayer.cornerRadius = cornerRadius
+        } else {
+            let gradLayer = CAGradientLayer()
+            gradLayer.frame = bounds
+            gradLayer.colors = [startColor.cgColor, endColor.cgColor]
+            gradLayer.cornerRadius = cornerRadius
+            layer.insertSublayer(gradLayer, at: 0)
+            gradientLayer = gradLayer
+        }
+    }
+    
+    // Nota: Para que el gradiente se ajuste al cambiar el tamaño del botón, es recomendable llamar a updateGradientLayer() desde layoutSubviews.
 }
